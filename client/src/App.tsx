7e2fc0e2d6263a960d4176119f78a845a57df34c
Yumi <ingyinmay1932003@gmail.com>
2025-12-18 14:25:12 +0630

    initial-set-up

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-pink-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-pink-600">MVP Cosemtic Store</h1>
+    </div>
+  );
+}
+
+export default App;
